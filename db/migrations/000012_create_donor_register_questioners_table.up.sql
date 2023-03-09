17838197e8e992c688b868ce27b38d6d164392cc
Taupik Pirdian <taupik.pirdian@salt.co.id>
2023-03-09 22:52:10 +0700

    new migration

--- db/migrations/000012_create_donor_register_questioners_table.up.sql
+++ db/migrations/000012_create_donor_register_questioners_table.up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE donor_register_questioners (
   id serial PRIMARY KEY,
-  code VARCHAR (150) NOT NULL,
-  userId INT UNSIGNED NOT NULL,
-  donorSchedulleId INT UNSIGNED NOT NULL,
-  isApprove INT DEFAULT (1),
-  donorProff VARCHAR (150) NULL, 
+  donorRegisterId INT UNSIGNED NOT NULL,
+  codeQuestion VARCHAR (150) NOT NULL,
+  title VARCHAR (150) NOT NULL, 
+  answer VARCHAR (150) NOT NULL, 
   updatedAt TIMESTAMP NULL,
   createdAt TIMESTAMP NULL
 );
